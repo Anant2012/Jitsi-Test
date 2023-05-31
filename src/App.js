017bf785c590e65e43e5299aa7698595f075e68f
Anant2012 <anant202002@gmail.com>
2023-05-31 11:59:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { JitsiMeeting } from "@jitsi/react-sdk";
+
+function App() {
+  return (
+    <div>
+      <>
+        <JitsiMeeting
+          roomName="ChatNepal"
+          configOverwrite={{
+            startWithAudioMuted: true,
+            disableModeratorIndicator: true,
+            startScreenSharing: true,
+            enableEmailInStats: false,
+            prejoinPageEnabled: false,
+          }}
+          interfaceConfigOverwrite={{
+            DISABLE_JOIN_LEAVE_NOTIFICATIONS: true,
+          }}
+          userInfo={{
+            displayName: "AMAN",
+          }}
+          getIFrameRef={(node) => (node.style.height = "1000px")}
+        />
+      </>
+    </div>
+  );
+}
+
+export default App;
